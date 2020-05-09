44c65b8c9a9b83191c56954be4d3c7774e42bd5a
frangoose <frangoose@gmail.com>
2020-05-09 01:31:22 -0700

    Initial commit, added .vimrc and vm_install_pkgs.sh

---
+++ vm_install_pkgs.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+IFS=' '
+
+pkgs="vim gcc make git net-tools"
+read -ra pkg_arr <<< "$pkgs"
+
+echo "The following packages will be installed using apt:"
+for i in "${pkg_arr[@]}"; do
+    echo $i
+done
+
+read -p "Do you wish to proceed?(y/n)" response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+    sudo apt update
+    sudo apt upgrade
+    sudo apt install $pkgs
+fi
